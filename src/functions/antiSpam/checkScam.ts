e734c8deb96c651681251bff8898824384a2b5eb
almostSouji <timoqueezle@gmail.com>
2021-11-11 00:08:14 +0100

    feat(scams): handle redirects

--- src/functions/antiSpam/checkScam.ts
+++ src/functions/antiSpam/checkScam.ts
@@ -1,9 +1,39 @@
 import { Client } from 'discord.js';
 import { SCAM_DOMAINS } from '../../utils/keys';
+import { logger } from '../../utils/logger';
+import { resolveRedirect } from '../../utils/resolveRedirect';
 
 export async function checkScam(client: Client, content: string): Promise<string[]> {
 	const redis = client.redis;
 
+	const urlRegex =
+		/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+
+	const trippedDomains = [];
+	let matches: any[] | null = [];
+
 	const scamDomains = await redis.smembers(SCAM_DOMAINS);
-	return scamDomains.filter((domain) => content.toLowerCase().includes(domain));
+
+	while ((matches = urlRegex.exec(content)) !== null) {
+		const url = matches[0];
+		const hit = scamDomains.find((d) => url.toLowerCase().includes(d));
+
+		if (hit) {
+			trippedDomains.push(hit);
+			continue;
+		}
+
+		try {
+			const resolved = await resolveRedirect(url);
+			const hit = scamDomains.find((d) => resolved.toLowerCase().includes(d));
+			if (hit) {
+				trippedDomains.push(hit);
+			}
+		} catch (e) {
+			const err = e as Error;
+			logger.error(err, 'Error while trying to resolve Redirect:');
+		}
+	}
+
+	return trippedDomains;
 }
